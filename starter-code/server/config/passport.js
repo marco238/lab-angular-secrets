092ecc76017fa1ecf7da3c0f9a9f814bb4cf1a55
Marco <marco238cuba@gmail.com>
2018-03-13 21:24:51 +0100

    feat: passport config done

--- starter-code/server/config/passport.js
+++ starter-code/server/config/passport.js
@@ -1,27 +1,40 @@
-const LocalStrategy = require("passport-local").Strategy;
-const User          = require("../models/user");
-const bcrypt        = require("bcrypt");
+const User = require('../models/user.model');
+const LocalStrategy = require('passport-local').Strategy;
+const ApiError = require('../models/api-error.model');
 
-module.exports = (passport) => {
-  passport.use(new LocalStrategy((username, password, next) => {
-    User.findOne({ username }, (err, user) => {
-      if (err) { return next(err); }
-      if (!user) { next(null, false, { message: "Incorrect username" }); }
-      if (!bcrypt.compareSync(password, user.password)) { next(null, false, { message: "Incorrect password" });
-    }
+module.exports.setup = (passport) => {
+  passport.serializeUser((user, next) => {
+    next(null, user._id);
+  });
 
-      return next(null, user);
-    });
+  passport.deserializeUser((id, next) => {
+    User.findById(id)
+      .then(user => {
+        next(null, user);
+      })
+      .catch(error => next(error));
+  });
+
+  passport.use('local-auth', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, (email, password, next) => {
+    User.findOne({ email: email })
+      .then(user => {
+        if (!user) {
+          next(null, user, { password: 'Invalid email or password' });
+        } else {
+          user.checkPassword(password)
+            .then(match => {
+              if (match) {
+                next(null, user);
+              } else {
+                next(null, null, { password: 'Invalid email or password' });
+              }
+            })
+            .catch(error => next(error));
+        }
+      })
+      .catch(error => next(error));
   }));
-
-  passport.serializeUser((user, cb) => {
-    cb(null, user.id);
-  });
-
-  passport.deserializeUser((id, cb) => {
-    User.findOne({ _id: id }, (err, user) => {
-      if (err) { return cb(err); }
-      cb(null, user);
-    });
-  });
-}
+};
